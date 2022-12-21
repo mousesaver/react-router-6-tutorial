16143f51f3ab44080eed0b6dc746f10657b0ddd2
mousesaver <zfan45@gmail.com>
2022-12-20 17:44:33 -0800

    React Router 6 - Error Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Products from './pages/Products'
+import Error from'./pages/Error'
 function App() {
   return (
     <BrowserRouter>
@@ -9,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/products" element={<Products />} />
+        <Route path="/*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   )
