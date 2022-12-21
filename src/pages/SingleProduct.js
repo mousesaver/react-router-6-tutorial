88c9928acd8fb2d96a09f325bbb3bde609514d6c
mousesaver <zfan45@gmail.com>
2022-12-20 23:04:07 -0800

    React Router 6 - useParams

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -1,7 +1,12 @@
+
+import { Link, useParams } from "react-router-dom"; 
 const SingleProduct = () => {
+  console.log(useParams())
+  const {productId} = useParams()
   return (
     <section className='section product'>
-      <h2>single product</h2>
+      <h2>single product: {productId}</h2>
+      <Link to='/products'>Go Back to Products</Link>
     </section>
   );
 };
