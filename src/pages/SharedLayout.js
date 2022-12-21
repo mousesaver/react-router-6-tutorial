801ef6d3a3ba3b80786cb584a251600f0dd1755d
mousesaver <zfan45@gmail.com>
2022-12-20 21:40:56 -0800

    React Router 6 - NavLink

--- src/pages/SharedLayout.js
+++ src/pages/SharedLayout.js
@@ -1,9 +1,10 @@
 import { Link, Outlet } from 'react-router-dom'
 import Navbar from '../Components/Navbar'
+import StyledNavbar from '../Components/StyledNavbar'
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <StyledNavbar />
       <Outlet />
     </>
   );
